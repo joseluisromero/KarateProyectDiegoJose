b82a01c7f626b2247ce85eac854d563523029f62
joromero <joromero@pichincha.com>
2025-06-19 19:29:12 -0500

    prueba commit  maquina banco

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,7 +1,7 @@
 function fn() {
   var config = {};
   // Configuración global de Karate
-  config.baseUrl = 'http://localhost:8080';
+  //config.baseUrl = 'http://localhost:8080';
   // Puedes agregar más variables globales aquí
   return config;
 }
